a788b2ca8a2e4ead440f8d05a43ea68cafec02ea
Javier Stifano <jstifano18@gmail.com>
2019-08-03 16:35:36 -0400

    Comprimiendo proyecto para bajar su peso en KB

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -28,16 +28,8 @@ const routes: Routes = [
         loadChildren: '../components/grupo-uno/grupo-uno.module#GrupoUnoModule'
       },
       {
-        path: 'grupo-nueve',
-        loadChildren: '../components/grupo-nueve/grupo-nueve.module#GrupoNueveModule'
-      },
-      {
         path: 'grupo-tres',
         loadChildren: '../components/grupo-tres/grupo-tres.module#GrupoTresModule'
-      },
-      {
-        path: 'grupo-trece',
-        loadChildren: '../components/grupo-trece/grupo-trece.module#GrupoTreceModule'
       },
       {
         path: 'mis-reservas',
@@ -48,10 +40,6 @@ const routes: Routes = [
         path: 'cruceros',
         loadChildren:
           '../components/grupo-ocho-cruceros/grupo-ocho-cruceros.module#GrupoOchoCrucerosModule'
-      },
-      {
-        path: 'flight-reservations',
-        loadChildren: '../components/grupo-doce-vuelos/flight-reservations/flight-reservations.module#FlightReservationsModule'
       },
       {
         path: 'administrar-hoteles',
@@ -71,22 +59,8 @@ const routes: Routes = [
           '../components/grupo-once-pago/grupo-once-pago.module#GrupoOncePagoModule'
       },
       {
-        path: 'restaurant-reservation',
-        loadChildren:
-          '../components/grupo-catorce-restaurant/grupo-catorce-restaurant.module#GrupoCatorceRestaurantModule'
-      },
-      {
         path: 'users',
         loadChildren: '../components/users/users-list/users-list.module#UsersListModule'
-      },
-
-      {
-        path: 'grupo-cinco',
-        loadChildren: '../components/grupo-cinco/grupo-cinco.module#GrupoCincoModule'
-      },
-      {
-        path: 'grupo-cuatro',
-        loadChildren: '../components/grupo-cuatro/grupo-cuatro.module#GrupoCuatroModule'
       }
     ]
   }
